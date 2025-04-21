e6e857e76e72a15b4b27baf2e6151a590ca26ccc
frayzkilla <frayzthamotion@gmail.com>
2025-04-21 15:09:24 +0800

    optimized sequence generator

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,4 @@
-from itertools import product
+from itertools import permutations, product
 from itertools import combinations
 
 def generate_subset_strings(k):
@@ -13,10 +13,22 @@ def generate_combinations(k, m):
     for combo in product(subsets, repeat=m):
         yield ' '.join(combo)
 
-def generate_sequences(k, n):
+def no_generate_sequences(k, n):
     base = list(product(range(k), repeat=n))
     max_length = k ** n  
     
     for length in range(1, max_length + 1):
         for sequence in product(base, repeat=length):
+            print(sequence)
             yield sequence
+            
+def generate_sequences(k, n):
+    base = list(product(range(k), repeat=n))
+    seen = set()
+
+    for length in range(1, len(base) + 1):
+        for combo in combinations(base, length):
+            sorted_combo = tuple(sorted(combo))
+            if sorted_combo not in seen:
+                seen.add(sorted_combo)
+                yield combo  
